16fa838747272d4a51c283320017b2a19137c967
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-06-10 17:56:28 +0700

    feat: Add more user routes

--- src/routes/user_routes.go
+++ src/routes/user_routes.go
@@ -27,29 +27,22 @@ func NewUserRoutes(router *gin.Engine) (*UserRoutes, error) {
 		userController: userService,
 	}, nil
 }
+func InitializeUserRoutes(router *gin.Engine) *gin.Engine {
+	userRoutes, err := NewUserRoutes(router)
+	if err != nil {
+		panic(err)
+	}
 
-func (u *UserRoutes) InitializeRoutes() {
-	u.baseRouter.GET("/", u.getUsers)
-	u.baseRouter.POST("/", u.createUser)
+	userRoutes.registerRoutes()
+	return router
 }
 
-// getUsers handles the GET /api/v1/users request
-// @Summary Get all users
-// @Description Get details of all users
-// @Tags users
-// @Accept  json
-// @Produce  json
-// @Success 200 {array} model.User
-// @Failure 500 {string} string
-// @Router /api/v1/users [get]
-func (u *UserRoutes) getUsers(ctx *gin.Context) {
-	u.userController.SetContext(ctx)
-	users, err := u.userController.GetUsers()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, err.Error())
-		return
-	}
-	ctx.JSON(http.StatusOK, users)
+func (u *UserRoutes) registerRoutes() {
+	u.baseRouter.POST("/create", u.createUser)
+	u.baseRouter.PATCH("/update", u.createUser)
+
+	u.baseRouter.GET("/get/:id", u.getUserByID)
+	u.baseRouter.GET("/get", u.getUsersByID)
 }
 
 // createUser handles the POST /api/v1/users request
@@ -78,12 +71,83 @@ func (u *UserRoutes) createUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, createdUser)
 }
 
-func InitializeUserRoutes(router *gin.Engine) *gin.Engine {
-	userRoutes, err := NewUserRoutes(router)
+// getUserByID handles the GET /api/v1/users/:id request
+// @Summary Get a user by ID
+// @Description Get a user by ID
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param id path string true "User ID"
+// @Success 200 {object} model.User
+// @Failure 400 {string} string
+// @Failure 404 {string} string
+// @Failure 500 {string} string
+// @Router /api/v1/users/{id} [get]
+func (u *UserRoutes) getUserByID(ctx *gin.Context) {
+	u.userController.SetContext(ctx)
+	id := ctx.Param("id")
+	user, err := u.userController.GetUserByID(id)
 	if err != nil {
-		panic(err)
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
+	if user == nil {
+		ctx.JSON(http.StatusNotFound, "User not found")
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
 
-	userRoutes.InitializeRoutes()
-	return router
+// getUsersByID handles the GET /api/v1/users request
+// @Summary Get a list of users by IDs
+// @Description Get a list of users by IDs
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param ids query []string true "User IDs"
+// @Success 200 {array} model.User
+// @Failure 400 {string} string
+// @Failure 404 {string} string
+// @Failure 500 {string} string
+// @Router /api/v1/users [get]
+func (u *UserRoutes) getUsersByID(ctx *gin.Context) {
+	u.userController.SetContext(ctx)
+	ids := ctx.QueryArray("ids")
+	users, err := u.userController.GetUsersByID(ids)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	if len(users) == 0 {
+		ctx.JSON(http.StatusNotFound, "Users not found")
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+// updateUser handles the PATCH /api/v1/users request
+// @Summary Update a user
+// @Description Update a user with the input payload
+// @Tags users
+// @Accept  json
+// @Produce  json
+// @Param user body model.User true "User"
+// @Success 200 {object} model.User
+// @Failure 400 {string} string
+// @Failure 404 {string} string
+// @Failure 500 {string} string
+// @Router /api/v1/users [patch]
+func (u *UserRoutes) updateUser(ctx *gin.Context) {
+	u.userController.SetContext(ctx)
+	var user model.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	updatedUser, err := u.userController.UpdateUser(user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx.JSON(http.StatusOK, updatedUser)
 }
